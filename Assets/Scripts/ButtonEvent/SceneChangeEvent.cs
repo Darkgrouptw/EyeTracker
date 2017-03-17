38ac3556b580362aa5a7d41e253bd52832ba2d8b
Darkgrouptw <darkgrouptw@gmail.com>
2017-03-18 00:54:45 +0800

    1. 第一個場景修正完成 UI 校正完成 2. 第二個場景也是 3. 測試影片放上去 4. 發現 A3執行上的問題

---
+++ Assets/Scripts/ButtonEvent/SceneChangeEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeEvent : MonoBehaviour
+{
+    void Start()
+    {
+        Application.OpenURL(@"http://answers.unity3d.com");
+    }
+
+    void Update()
+    {
+        // 離開程式
+        if(Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+
+    
+    public void A1ToA2()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void A2ToA3()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+}
