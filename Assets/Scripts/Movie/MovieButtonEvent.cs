c6c76e06f46f120ac5ef20bb7fcdef770c1ba353
Darkgrouptw <darkgrouptw@gmail.com>
2017-03-21 15:30:29 +0800

    1. 使用最先版 Unity 2. 寫了 A3 上下兩條 => 移動動畫 & 消失的動畫

--- Assets/Scripts/Movie/MovieButtonEvent.cs
+++ Assets/Scripts/Movie/MovieButtonEvent.cs
@@ -1,12 +1,149 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MovieButtonEvent : MonoBehaviour
 {
 
-    //private bool
+    ////////////////////////////////////////////////////
+    // Chromecast相關
+    ////////////////////////////////////////////////////
+    private bool IsChromecast = false;
+
+
+
+    ////////////////////////////////////////////////////
+    // 播放欄相關
+    ////////////////////////////////////////////////////
+    [Header("========== 播放欄相關的 ==========")]
+    public RectTransform Navbar;                        // 上面那欄
+    public RectTransform Footer;                        // 下面那欄
+    
+    #region 座標的參數
+    private int NavStartPosY, FooterStartPosY;          // 上下一開始的位置
+    private int NavbarEndPosY, FooterEndPosY;           // 上下移動終止的地方
+    private int NavbarHeight, FooterHeight;             // 上下的寬度
+    private float NavbarMoveSpeed, FooterMoveSpeed;        // 上下的距離
+    #endregion
+
+    private int HiddenStatus = 0;                       // 0 => 沒有要播動畫、1 => 消失動畫、2 => 消失的狀態、3 => 顯示動畫
+    private float HiddenCounter = 0;                    // 計數器
+    private const int HiddenTime = 6;                   // 6 秒沒有碰到，就會消失
+    private const int HiddenProgress = 1;               // 消失 & 顯示的時間
+
+
+    void Start()
+    {
+        // 初始化給的寬
+        RectTransform navbarHover = Navbar.GetComponentsInChildren<Transform>()[2].GetComponent<RectTransform>();
+        RectTransform footerHover = Footer.GetComponentsInChildren<Transform>()[2].GetComponent<RectTransform>();
+
+        // 寬度
+        NavbarHeight = (int)navbarHover.sizeDelta.y;
+        FooterHeight = (int)footerHover.sizeDelta.y;
+
+        // 上方
+        NavStartPosY = (int)Navbar.transform.localPosition.y;
+        NavbarEndPosY = NavStartPosY + NavbarHeight;
+
+        // 下方
+        FooterStartPosY = (int)Footer.transform.localPosition.y;
+        FooterEndPosY = FooterStartPosY - FooterHeight;
+    }
+
+    void Update()
+    {
+        switch (HiddenStatus)
+        {
+            case 0:
+                HiddenCounter += Time.deltaTime;
+
+                // 判斷是否閒適夠久，如果夠久，就開始縮起來
+                if (HiddenCounter >= HiddenTime)
+                {
+                    HiddenCounter = 0;
+                    HiddenStatus = 1;
+
+                    // 從頭到目標的速度
+                    NavbarMoveSpeed = NavbarEndPosY - Navbar.transform.localPosition.y;
+                    FooterMoveSpeed = FooterEndPosY - Footer.transform.localPosition.y;
+                }
+                break;
+            case 1:
+                HiddenCounter += Time.deltaTime;
+
+                // 判斷隱藏的條件
+                if (HiddenCounter >= HiddenProgress)
+                {
+                    HiddenStatus = 2;
+                    HiddenCounter = 0;
+
+                    Navbar.transform.localPosition = new Vector3(0, NavbarEndPosY, 0);
+                    Footer.transform.localPosition = new Vector3(0, FooterEndPosY, 0);
+                }
+                else
+                {
+                    //float Progress = HiddenCounter / HiddenProgress;
+                    Navbar.transform.localPosition += new Vector3(0, NavbarMoveSpeed, 0) / HiddenProgress * Time.deltaTime;
+                    Footer.transform.localPosition += new Vector3(0, FooterMoveSpeed, 0) / HiddenProgress * Time.deltaTime;
+                }
+                break;
+
+            case 2:
+                // 隱藏，所以不用動作
+                break;
+            case 3:
+                HiddenCounter += Time.deltaTime;
+
+                if(HiddenCounter >= HiddenProgress)
+                {
+                    HiddenStatus = 0;
+                    HiddenCounter = 0;
+
+                    Navbar.transform.localPosition = new Vector3(0, NavStartPosY, 0);
+                    Footer.transform.localPosition = new Vector3(0, FooterStartPosY, 0);
+                }
+                else
+                {
+                    //float Progress = HiddenCounter / HiddenProgress;
+                    Navbar.transform.localPosition -= new Vector3(0, NavbarMoveSpeed, 0) / HiddenProgress * Time.deltaTime;
+                    Footer.transform.localPosition -= new Vector3(0, FooterMoveSpeed, 0) / HiddenProgress * Time.deltaTime;
+                }
+                break;
+
+        }
+    }
+
+    public void BarHover()
+    {
+        switch (HiddenStatus)
+        {
+            case 0:
+                // 重製 Counter
+                HiddenCounter = 0;
+                break;
+            case 1:
+            case 2:
+                // 從現在到開始的速度
+                NavbarMoveSpeed = Navbar.transform.localPosition.y - NavStartPosY;
+                FooterMoveSpeed = Footer.transform.localPosition.y - FooterStartPosY;
+
+                HiddenStatus = 3;
+                HiddenCounter = 0;
+                break;
+            case 3:
+                // 正在復原，所以不用做事
+                break;
+        }
+    }
+
+
     public void ChromecastButton()
     {
+        if(!IsChromecast)
+        {
 
+        }
     }
+
 }
--- Assets/Scripts/Movie/MovieManger.cs
+++ Assets/Scripts/Movie/MovieManger.cs
@@ -10,15 +10,17 @@ public class MovieManger : MonoBehaviour
 
     public RectTransform FrontBar;                                  // 前面那條 bar
     public RectTransform BlueDot;                                   // 藍色的點
+    public Text timeLabel;                                          // 時間的 Label
 
-    private float MovieLength = 0;                                  // 影片長度
+    private const float MovieLength = 417;                          // 影片長度
     private float MoviePlayTime = 0;                                // 影片目前播出的時間
+    private const float MovieLoadingLength = 4;                     // 影片的讀取時間
 
 
     // 座標參數
     private const int StartPosX = -850;                             // 開始的 X 位置
     private const int EndPosX = 650;                                // 結束的 X 位置
-    private const int PosY = -630;                                  // Y 的位置
+    private const int PosY = -632;                                  // Y 的位置
 
 	void Start ()
     {
@@ -42,17 +44,20 @@ public class MovieManger : MonoBehaviour
         if(movie.isPlaying)
         {
             MoviePlayTime += Time.deltaTime;
-            BlueDot.transform.localPosition = BlueDotLocation();
-            FrontBar.transform.localScale = FrontBarRatio();
+            BlueDot.localPosition = BlueDotLocation();
+            FrontBar.localScale = FrontBarRatio();
+            timeLabel.text = MovieTimeText();
         }
         #endregion
     }
 
+
+    #region 影片進度相關的 Function
     // 回傳藍點應該的位置
     private Vector3 BlueDotLocation()
     {
         const float lineWidth = (float)(EndPosX - StartPosX);
-        return new Vector3(lineWidth * MoviePlayRatio(), PosY, 0);
+        return new Vector3(lineWidth * MoviePlayRatio() + StartPosX, PosY, 0);
     }
 
     // 回傳藍點的比例
@@ -61,9 +66,32 @@ public class MovieManger : MonoBehaviour
         return new Vector3(MoviePlayRatio(), 1, 1);
     }
 
+    private string MovieTimeText()
+    {
+        int secs = (int)(MoviePlayTime - MovieLoadingLength);
+
+        // 要扣掉 loading 的時間
+        if (secs < 0)
+            secs = 0;
+
+        int mins = secs / 60;                       // Unity 的 int 會無條件捨去
+
+        return TimeFormat(mins) + ":" + TimeFormat(secs);
+    }
+
+    private string TimeFormat(int num)
+    {
+        if (num < 10)
+            return "0" + num.ToString();
+        return num.ToString();
+    }
+
     // 回傳影片的百分比
     private float MoviePlayRatio()
     {
-        return MoviePlayTime / MovieLength;
+        if (MoviePlayTime <= MovieLoadingLength)
+            return 0;
+        return (MoviePlayTime - MovieLoadingLength) / (MovieLength - MovieLoadingLength);
     }
+    #endregion
 }
